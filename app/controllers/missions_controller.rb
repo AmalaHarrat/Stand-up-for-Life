3c9fb9edfec9bd19dc423d48042435e2a895c0a2
nicolas naas <nicolas_naas@outlook.fr>
2022-12-02 15:35:43 +0100

    search bar

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,6 +1,10 @@
 class MissionsController < ApplicationController
   def index
-    @missions = Mission.all
+    if params[:query].present?
+      @missions = Mission.where(city: params[:query])
+    else
+      @missions = Mission.all
+    end
   end
 
   def show
